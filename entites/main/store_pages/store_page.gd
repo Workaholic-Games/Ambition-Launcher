2ca3eac4eb2d48d3c332c37934ae378ee7fd5f17
bulbatf7-lgtm <bulbatf7@gmail.com>
2026-04-15 19:39:05 -0700

    uninstall stuff

--- entites/main/store_pages/store_page.gd
+++ entites/main/store_pages/store_page.gd
@@ -23,35 +23,52 @@ func _ready() -> void:
 
 func _on_versions_item_selected(index: int) -> void:
 	selected_link = index
+	check_for_uninstall()
 
 
-
+# Installation and Uninstallation
 func _on_install_pressed() -> void:
-	if can_download == true:
+	if can_download == true and $Install.text == "Install":
 		$HTTPRequest.download_file = "user://" + version_file_names.get(selected_link) + ".exe"
 		$HTTPRequest.request(version_links.get(selected_link))
 		can_download = false
 		$ProgressBar.visible = false
+	elif can_download == true and $Install.text == "Uninstall":
+		DirAccess.remove_absolute("user://" + version_file_names.get(selected_link) + ".exe")
+		$Install.text = "Install"
 
 
 func _on_back_pressed() -> void:
 	visible = false
 
 
-
+# Progress Bar function
 func _physics_process(_delta: float) -> void:
 	if can_download == false:
 		$ProgressBar.visible = true
 		var downloaded = $HTTPRequest.get_downloaded_bytes()
 		var total = $HTTPRequest.get_body_size()
-				
 		if total > 0:
 			$ProgressBar.value = (float(downloaded) / total) * 100
 
 
 
+# Download Complete code
 func _on_http_request_request_completed(_result: int, _response_code: int, _headers: PackedStringArray, _body: PackedByteArray) -> void:
 	var absolute_path = ProjectSettings.globalize_path("user://" + version_file_names.get(selected_link) + ".exe")
 	OS.shell_open(absolute_path)
 	can_download = true
 	$ProgressBar.visible = false
+	check_for_uninstall()
+
+
+
+# Crap to check if you can Uninstall the game
+func _on_visibility_changed() -> void:
+	check_for_uninstall()
+
+
+func check_for_uninstall():
+	if FileAccess.file_exists("user://" + version_file_names.get(selected_link) + ".exe"):
+		$Install.text = "Uninstall"
+		
