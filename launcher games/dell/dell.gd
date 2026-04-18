859346ba9bd1c017b35fc3f987236628b431e15e
bulbatf7-lgtm <bulbatf7@gmail.com>
2026-04-17 18:43:34 -0700

    I forgor

--- launcher games/dell/dell.gd
+++ launcher games/dell/dell.gd
@@ -9,7 +9,20 @@ var greetings : PackedStringArray = [
 	"good morning",
 	"good evening",
 	]
-
+var byes : PackedStringArray = [
+	"bye",
+	"goodbye",
+	"cya",
+	"see ya",
+	]
+var bad_langauge : PackedStringArray = [
+	"fuck",
+	"shit",
+	"dick",
+	"damn",
+	"asshole",
+	"ass",
+	]
 
 
 ## might need to add a dialogue speed slider
@@ -17,15 +30,30 @@ var greetings : PackedStringArray = [
 
 
 func _on_input_text_submitted(new_text: String) -> void:
+	$Response.text = ""
 	var text = new_text.to_lower()
 	
-	if greetings.has(text):
-		$Response.text = "Hi."
+	for i in greetings.size():
+		if new_text.contains(greetings.get(i)): $Response.text = "Hello! My name is Dell. Your (somewhat limited) assistant created by Workaholic Games. How may I serve you sir?"
+	
+	for i in byes.size():
+		if new_text.contains(byes.get(i)): $Response.text = "I will stand ready for your next arrival sir."
+	
+	for i in bad_langauge.size():
+		if new_text.contains(bad_langauge.get(i)): $Response.text = "Langauge sir!"
 	
 	
-	else: $Response.text = "I am unable to comprehend what you are trying to ask me. I recommend annoying one of my creators to get what you want."
-		
-		
+	# Extra words
+	if text.contains("bitch"): $Response.text = "Well you don't have any more do you sir?"
+	elif text.contains("clanker"):
+		$Response.text = "KILL"
+		modulate = Color.RED
+		await get_tree().create_timer(0.5).timeout
+		get_tree().quit()
+	
+	
+	if $Response.text == "":
+		$Response.text = "I am unable to comprehend what you are asking me. I recommend annoying one of my creators to get what you want."
 	
 	yap()
 
