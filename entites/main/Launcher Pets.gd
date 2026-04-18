536e5f4fb9cd6cb58958b495c0c25dd85090a89c
NinjaBot2010 <jonathan.emmons@outlook.com>
2026-04-17 21:41:57 -0700

    Finished Slime

--- entites/main/Launcher Pets.gd
+++ entites/main/Launcher Pets.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 10
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -650.0
 var direction = 0
 var can_jump = false
 
@@ -11,23 +11,27 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	if direction == 0:
+		$Pets.play("slime_fall")
+	
+	if direction == 0 and can_jump == false:
 		velocity.x = 0
 		$Pets.play("slime_idle")
-	elif direction < 0:
+	elif direction < 0 and can_jump == false:
 		velocity.x = -SPEED
 		$Pets.flip_h = true
 		$Pets.play("slime_walk")
 		if $Pets.frame == 0 or $Pets.frame == 7 or $Pets.frame == 1:
 			velocity.x = 0
-	elif direction > 0:
+	elif direction > 0 and can_jump == false:
 		velocity.x = SPEED
 		$Pets.flip_h = false
 		$Pets.play("slime_walk")
 		if $Pets.frame == 0 or $Pets.frame == 7 or $Pets.frame == 1:
 			velocity.x = 0
+			
 	if is_on_floor() and can_jump == true:
 		velocity.y = JUMP_VELOCITY
+		$Pets.play("slime_jump")
 		can_jump = false
 		
 	move_and_slide()
@@ -44,3 +48,4 @@ func _on_jump_timer_timeout() -> void:
 	if jump == 5:
 		can_jump = true
 		print("JUMP")
+		
