6634babad1d6816c8f689e7a5f76202b7fa7d987
bulbatf7-lgtm <bulbatf7@gmail.com>
2026-06-14 19:11:04 -0700

    avatars

--- avatar_instance.gd
+++ avatar_instance.gd
@@ -1,7 +1,13 @@
 extends TextureButton
 
-@export var avatar : Texture = null
 @export var menu : Panel 
 @export var index : int
+
+
+
+func _ready() -> void:
+	texture_normal = menu.default_avatars[index]
+
 func _on_pressed() -> void:
-	Main.launcher_data.avatar = menu.default_avatars[index]
+	Main.launcher_data.avatar = index
+	menu.update_avatar()
--- entites/main/avatar.gd
+++ entites/main/avatar.gd
@@ -1,16 +1,45 @@
 extends Panel
 
+var avatar_instance = load("res://entites/main/avatar_instance.tscn")
 var default_avatars =  [
-	load("res://branding/gobletv2_upscaled.png"),
-	load("res://branding/godot_v2.png"),
+	load("res://avatars/fnab1_arthur.png"),
+	load("res://avatars/fnab1_bulba.png"),
+	load("res://avatars/fnab1_charlie.png"),
+	load("res://avatars/fnab1_claire.png"),
+	load("res://avatars/fnab1_gigabot.png"),
+	load("res://avatars/fnab1_mark.png"),
+	load("res://avatars/fnab2_bobbleheads.png"),
+	load("res://avatars/fnab2_bulba.png"),
+	load("res://avatars/fnab2_charlie.png"),
+	load("res://avatars/fnab2_claire.png"),
+	load("res://avatars/fnab2_gavin.png"),
+	load("res://avatars/fnab2_gigabot.png"),
+	load("res://avatars/fnab2_mark.png"),
+	load("res://avatars/fnab2_nightguard.png"),
+	load("res://avatars/fnab2_ninjabot.png"),
+	load("res://avatars/fnab2_sawyer.png"),
+	load("res://avatars/fnab2_theoriginal.png"),
+	load("res://avatars/seedling.png"),
+	load("res://avatars/tater.png"),
 ]
+
 
 
 func _ready() -> void:
 	if Main.launcher_data.username == "":
 		var random_avatar = randi_range(0, default_avatars.size()-1)
-		$Avatar.texture_normal = default_avatars[random_avatar]
 		Main.launcher_data.avatar = random_avatar
-	else:
+	
+	for i in default_avatars.size() - 1:
+		var new_avatar = avatar_instance.instantiate()
+		new_avatar.index = i
+		new_avatar.menu = self
+		
+		$"../User/Avatar Selection/ScrollContainer/GridContainer".add_child(new_avatar)
+	
 		$Avatar.texture_normal = default_avatars[Main.launcher_data.avatar]
 		$"../User/Avatar/Avatar".texture = default_avatars[Main.launcher_data.avatar]
+
+func update_avatar():
+	$Avatar.texture_normal = default_avatars[Main.launcher_data.avatar]
+	$"../User/Avatar/Avatar".texture = default_avatars[Main.launcher_data.avatar]
