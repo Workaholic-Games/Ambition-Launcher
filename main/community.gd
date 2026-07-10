d33f4993762ff3d5996845199140d4979fd52b7e
NinjaBot2010 <jonathan.emmons@outlook.com>
2026-07-09 17:52:13 -0700

    added secret lock ;)

--- main/community.gd
+++ main/community.gd
@@ -1,14 +1,21 @@
 extends Control
-
+var count = 0
+var count2 = 0
 
 
 func _on_featured_art_pressed() -> void:
 	hide_community()
 	$Featured.visible = true
 func _on_install_mods_pressed() -> void:
-	hide_community()
+	count += 1
+	if count == 1000:
+		$Lock.hide()
+	#hide_community()
 func _on_modding_guide_pressed() -> void:
-	hide_community()
+	count2 += 1
+	if count2 == 1000:
+		$Lock2.hide()
+	#hide_community()
 
 
 
